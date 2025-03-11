409127e74422074ff816c6f8b8828192ba1c2db1
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-11 21:42:39 +0100

    Add access modifiers to variables

--- src/text_generator.py
+++ src/text_generator.py
@@ -4,12 +4,12 @@ import random
 
 class TextGenerator:
     def __init__(self):
-        self.quotes = {}
-        self.quote = {}
+        self.__quotes = {}
+        self.__quote = {}
 
     def fetch_text(self):
         res = req.get('https://dummyjson.com/quotes/?limit=10')
-        self.quotes = res.json()['quotes']
+        self.__quotes = res.json()['quotes']
 
     def get_random_quote(self):
-        self.quote = random.choice(self.quotes)
+        self.__quote = random.choice(self.__quotes)
