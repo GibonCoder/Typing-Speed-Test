00795bdb09c7050644387cf410a7a5e5659ae0cc
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-11 21:46:54 +0100

    Create method for configuring window

--- src/window.py
+++ src/window.py
@@ -6,3 +6,6 @@ class Window:
     def __init__(self):
         self.__root = tk.Tk()
 
+    def __configure_window(self):
+        self.__root.geometry("1200x800")
+        self.__root.title("Speed Typing Test")
