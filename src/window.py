1e1c4add3c967c35de2ec65fe6e95dfabd5f5b55
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-11 21:50:08 +0100

    Create text fields

--- src/window.py
+++ src/window.py
@@ -5,6 +5,8 @@ from tkinter import ttk
 class Window:
     def __init__(self):
         self.__root = tk.Tk()
+        self.__text = tk.Text()
+        self.__user_input = tk.Text()
 
     def __configure_window(self):
         self.__root.geometry("1200x800")
