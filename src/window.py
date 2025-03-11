6ac83b99dd2777d570c43d3c075bd2327874b7fc
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-11 21:56:20 +0100

    Create Label element

--- src/window.py
+++ src/window.py
@@ -5,9 +5,15 @@ from tkinter import ttk
 class Window:
     def __init__(self):
         self.__root = tk.Tk()
-        self.__text = tk.Text()
+        self.__title = ttk.Label(text="Test Your Typing Speed")
+        self.__text = tk.Text(state='disabled')
         self.__user_input = tk.Text()
 
     def __configure_window(self):
         self.__root.geometry("1200x800")
         self.__root.title("Speed Typing Test")
+
+    def __place_widgets(self):
+        pass
+
+
