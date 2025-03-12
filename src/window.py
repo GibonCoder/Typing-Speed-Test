edeb49450e4aa3fef8ba0eff21369871058eec20
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-12 15:48:16 +0100

    Create Label widgets

--- src/window.py
+++ src/window.py
@@ -6,8 +6,12 @@ class Window:
     def __init__(self):
         # Window
         self.__root = tk.Tk()
+        # Widgets
         self.__title = ttk.Label(text="Test Your Typing Speed")
+        __label_left = tk.Label(self.__root, fg='grey')
+        __label_right = tk.Label(self.__root, fg='grey')
         # Variables
+        self._split_point = 0
         self._text = None
         self._user_input = None
 
@@ -17,6 +21,7 @@ class Window:
 
     def place_widgets(self):
         self.__title.grid(row=0, column=1)
+        self.
 
     def run_window(self):
         self.__root.mainloop()
