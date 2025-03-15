1100352c601163ef7c4f52762bebbeecac640230
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-15 14:21:10 +0100

    Rewrite configure_time method to SpeedTest class

--- src/speed_test.py
+++ src/speed_test.py
@@ -2,4 +2,9 @@ class SpeedTest:
     def __init__(self):
         self._split_point = 0
         self._write_able = True
-        self.passed_seconds = 0
+        self._passed_seconds = 0
+
+    def config_time(self, window):
+        self._passed_seconds = 0
+        window.after(60000, self._stop_test)
+        window.after(1000, self._add_second)
