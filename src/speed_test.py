3d33634cf5733ddea266f8dab2eb5a06bc494bee
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-15 14:49:54 +0100

    Rewrite add_second method to SpeedTest class

--- src/speed_test.py
+++ src/speed_test.py
@@ -4,7 +4,14 @@ class SpeedTest:
         self._write_able = True
         self._passed_seconds = 0
 
-    def config_time(self, window):
+    def config_time(self, window, time_lbl):
         self._passed_seconds = 0
         window.after(60000, self._stop_test)
-        window.after(1000, self._add_second)
+        window.after(1000, lambda: self._add_second(window, time_lbl))
+
+    def _add_second(self, window, time_lbl):
+        self._passed_seconds += 1
+        time_lbl.configure(text=f'{self._passed_seconds}')
+
+        if self._write_able:
+            window.after(1000, lambda: self._add_second(window, time_lbl))
